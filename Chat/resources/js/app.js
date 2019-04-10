5d1379daa17b1c675e26dbda5ce9d80380881ff6
nguyenkhanh1997 <32681079+nguyenkhanh1997@users.noreply.github.com>
2019-04-10 16:40:37 +0700

    First Commit

---
+++ Chat/resources/js/app.js
@@ -0,0 +1,94 @@
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+// Vue.component('chat', require('./components/Chat.vue'));
+
+import Chat from './components/Chat.vue';
+import ChatComposer from './components/ChatComposer.vue';
+import OnlineUser from './components/OnlineUser.vue';
+
+Vue.component('chat', Chat);
+Vue.component('chat-composer', ChatComposer);
+Vue.component('onlineuser', OnlineUser);
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        chats: '',
+        onlineUsers: ''
+    },
+    created() {
+        const userId = $('meta[name="userId"]').attr('content');
+        const friendId = $('meta[name="friendId"]').attr('content');
+
+        if (friendId != undefined) {
+            axios.post('./getChat/' + friendId).then((response) => {
+                this.chats = response.data;
+            })
+
+            Echo.private('Chat.' + friendId + '.' + userId)
+                .listen('BroadcastChat', (e) => {
+                    document.getElementById('ChatAudio').play();
+                    this.chats.push(e.chat);
+                });
+        }
+
+        if (userId != 'null') {
+            Echo.join('Online')
+                .here((users) => {
+                    this.onlineUsers = users;
+                })
+                .joining((user) => {
+                    this.onlineUsers.push(user);
+                })
+                .leaving((user) => {
+                    this.onlineUsers = this.onlineUsers.filter((u) => {u != user});
+                });
+        }    
+    }
+});
+
+
+// Bulma NavBar Burger Script
+document.addEventListener('DOMContentLoaded', function () {
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+        
+        // Add a click event on each of them
+        $navbarBurgers.forEach(function ($el) {
+            $el.addEventListener('click', function () {
+                
+                // Get the target from the "data-target" attribute
+                let target = $el.dataset.target;
+                let $target = document.getElementById(target);
+                
+                // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+                $el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+                
+            });
+        });
+    }
+    
+});
+
+
+
+require('./bulma-extensions');
---
+++ Chat/resources/js/components/Chat.vue
@@ -0,0 +1,63 @@
+<style scoped>
+    .panel-block {
+        flex-direction: column;
+        border: none;
+    }
+    .chat {
+        width: 100%;
+        padding: 20px;
+        box-shadow: inset 0 0 20px 0 grey;
+        margin-bottom: 20px;
+        border: 1px solid grey;
+        max-height: 600px;
+        overflow-x: auto;
+    }
+    .chat .chat-right, .chat .chat-left {
+        border-radius: 10px;
+        max-width: 70%;
+        box-shadow: 0 0 8px 0px grey;
+        padding: 8px;
+        margin: 10px;
+    }
+    .chat-right {
+        float: right;
+        background-color: #3341e8;
+        color: white;
+    }
+    .chat-left {
+        float: left;
+    }
+    .no-message {
+        height: 200px;
+        display: flex;
+        align-items: center;
+    }
+</style>
+
+<template>
+    <div class="panel-block">
+    	<div class="chat" v-if="chats.length != 0">
+    		<div v-for="chat in chats" style="overflow: auto">
+    			<div class="chat-right" v-if="chat.user_id == userid">
+    				{{ chat.chat }}
+    			</div>
+    			<div class="chat-left" v-else>
+    				{{ chat.chat }}
+    			</div>
+    		</div>
+    	</div>
+    	<div v-else class="no-message">
+    		There are no messages
+    	</div>
+    	<chat-composer v-bind:userid="userid" v-bind:chats="chats" v-bind:friendid="friendid"></chat-composer>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['chats' ,'userid', 'friendid'],
+        mounted() {
+			console.log('Chat mounted');
+		}
+    }
+</script>
